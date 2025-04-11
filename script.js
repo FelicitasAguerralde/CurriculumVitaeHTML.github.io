4903d5061f067c30732fd594823fd864ed16e91d
Felicitas <felicitas.aguerralde@gmail.com>
2025-04-11 19:16:19 -0300

    Agregue efectos y acomode colores

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*-----------------Cards images--------------------*/
 const cards = document.querySelectorAll('.imagesCard');
 
 cards.forEach((card, index) => {
@@ -8,5 +9,29 @@ cards.forEach((card, index) => {
   card.style.backgroundImage = `url(${imageUrl})`;
   card.style.backgroundSize = 'cover'; 
   card.style.backgroundRepeat = 'no-repeat'; 
-  //card.style.backgroundSize = 'contain'; 
+});
+
+/*-----------------Nav Scroll Efect---------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+  const navElement = document.getElementById('nav');
+  const originalOpacity = getComputedStyle(navElement).opacity;
+
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > 50) {
+      navElement.style.opacity = '0.5';
+    } else {
+      navElement.style.opacity = originalOpacity;
+    }
+  });
+
+navElement.addEventListener('mouseover', function() {
+  navElement.style.opacity = originalOpacity; // Vuelve a la opacidad original al pasar el ratón
+});
+
+navElement.addEventListener('mouseout', function() {
+  // Vuelve a opacar si el usuario ha scrolleado
+  if (window.scrollY > 50) {
+    navElement.style.opacity = '0.7'; // Misma opacidad que al scrollear
+  }
+});
 });
